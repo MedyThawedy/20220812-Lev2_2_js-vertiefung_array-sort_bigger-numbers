ef77ea83d313561c4f6e3c0e3cbc56e71ef1f656
MD <dhmedy@googlemail.com>
2022-08-12 15:10:14 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+console.log(numArray2);
+
+let sortedNumArray2 = numArray2;
+sortedNumArray2.sort(function (a, b) { return a - b });
+console.log(sortedNumArray2);
